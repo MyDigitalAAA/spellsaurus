cd1951fde027c44322d202b35cd44a877673c773
Alexis <35.alexis.pele@gmail.com>
2020-07-02 23:05:47 +0200

    - Hid some tables for users

--- models/user-model.js
+++ models/user-model.js
@@ -3,6 +3,7 @@ const bookshelf = require('../database/bookshelf').bookshelf
 
 let User = bookshelf.Model.extend({
     tableName: 'user',
+    hidden: ['id', 'password']
 })
 
 module.exports = bookshelf.model('User', User)
