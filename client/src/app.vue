7711816a71d325f601c42d5108e5c447fef5981c
Alexis <35.alexis.pele@gmail.com>
2020-07-17 21:02:13 +0200

    - Reworked client structure and optimized fonts

--- client/src/app.vue
+++ client/src/app.vue
@@ -12,3 +12,10 @@ export default {
   name: 'app'
 }
 </script>
+
+<style lang="scss" scoped>
+#srs {
+    min-height: 100vh;
+    background: url('/img/bg1.png') center center fixed repeat;
+}
+</style>
--- client/src/pages/index-page.vue
+++ client/src/pages/index-page.vue
@@ -5,7 +5,7 @@
                 <h1 class="title text-dark mb-4 font-display line-height-100">"C'est magique, ta gueule."</h1>
                 <div class="lead font-display font-weight-bold">— N'importe quel MJ, une fois dans sa vie</div>
                 <hr class="w-50">
-                <div class="lead"><span class="font-display" style="font-weight:700;">Auracle</span> est une base de données publique en ligne pour sortilèges, en soutien au jeu de rôle sur table <span class="font-weight-bold text-secondary">Leïm</span>.</div>
+                <div class="lead"><span class="font-display">Auracle</span> est une base de données publique en ligne pour sortilèges, en soutien au jeu de rôle sur table <span class="font-weight-bold text-secondary">Leïm</span>.</div>
             </main>
         </section>
     </div>
@@ -24,8 +24,13 @@ section {
         width: 1000px;
         .title {
             font-size: 5rem;
-            font-weight: 700;
+            font-weight: 900;
             text-align: center;
+        }
+        .lead {
+            .font-display {
+                font-weight: 700;
+            }
         }
     }
 }
