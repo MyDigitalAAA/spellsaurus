0178f15bf19dcc2f7f1399c975a6abffc1905996
Alexis <35.alexis.pele@gmail.com>
2020-12-18 11:58:16 +0100

    - Added timeline page (will need to be redone anyways)

--- client/src/pages/timelines/timeline-page.vue
+++ client/src/pages/timelines/timeline-page.vue
@@ -1,12 +1,18 @@
 <template>
-    <div class="container-fluid p-4" id="spell-container">
-        <h1 class="display-3 font-display mb-3">Chronologie</h1>
-    </div>
+  <div class="container-fluid p-4" id="spell-container">
+    <h1 class="display-3 font-display mb-3">Chronologie</h1>
+    <timeline/>
+  </div>
 </template>
 
 <script>
+import Timeline from '~/components/timeline/timeline'
+
 export default {
-    name: 'timeline-page',
+  name: 'timeline-page',
+  components: {
+    'timeline': Timeline,
+  }
 }
 </script>
 
