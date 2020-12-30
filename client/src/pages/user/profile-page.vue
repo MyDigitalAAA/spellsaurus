dde2e63b6149cd00d198a2b55b27d296328bef3d
Alexis <35.alexis.pele@gmail.com>
2020-12-30 22:17:28 +0100

    Temporary test ground for updating user form

--- client/src/pages/user/profile-page.vue
+++ client/src/pages/user/profile-page.vue
@@ -9,15 +9,35 @@
     </h2>
     <span>Membre depuis le {{ registered_date }}</span>
 
+    <ul class="nav nav-tabs mt-3 mb-3" id="tabs-tab" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link active" id="tabs-home-tab" data-toggle="pill" href="#tabs-home" role="tab" aria-controls="tabs-home" aria-selected="true">Mes sorts</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" id="tabs-contact-tab" data-toggle="pill" href="#tabs-contact" role="tab" aria-controls="tabs-contact" aria-selected="false">Paramètres</a>
+      </li>
+    </ul>
+    <div class="tab-content" id="tabs-tabContent">
+      <div class="tab-pane fade show active" id="tabs-home" role="tabpanel" aria-labelledby="tabs-home-tab">...</div>
+      <div class="tab-pane fade" id="tabs-contact" role="tabpanel" aria-labelledby="tabs-contact-tab">
+        <update-form :user="user"/>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
 
+import UpdateForm from '~/components/user/profile/update-form.vue'
+
 export default {
   name: 'profile-page',
   metaInfo: {
     titleTemplate: '%s - Profil'
+  },
+  components: {
+    UpdateForm,
   },
   data() {
     return {
